0329981d07b48910a921d6a9fc03e1bf8975b593
Hannes Wadman <hannes.wadman@gmail.com>
2024-04-06 14:42:09 +0200

    Add struct creation macro

--- utils/menu.c
+++ utils/menu.c
@@ -1,10 +1,13 @@
 
-/* \file menu.c
+/*! \file menu.c
  *
  * \brief Functions and constants pertaining to printing the main menu
  *
  * A file that contains the string constants used for printing to screen,
- * functions that will paint menus and handle input
+ * functions that will paint menus and handle input. This documentation will
+ * refer to banners and options. A banner is most commonly a larger ascii art
+ * indicating the name of the current menu, whereas options are just a series of
+ * strings that indicate the possible choices in the menu
  */
 
 #include <assert.h>
@@ -16,34 +19,30 @@
 #include "menu.h"
 #include "utf8.h"
 
+// clang-format off
 const char* const title[7] = {
-    " ___________  _______   _______   ___      ___       _______       __     "
-    "  ___      ___   _______  \n",
-    "(\"     _   \")/\"     \"| /\"      \\ |\"  \\    /\"  |     /\" _   \"|  "
-    "   /\"\"\\     |\"  \\    /\"  | /\"     \"| \n",
-    " )__/  \\\\__/(: ______)|:        | \\   \\  //   |    (: ( \\___)    /   "
-    " \\     \\   \\  //   |(: ______) \n",
-    "    \\\\_ /    \\/    |  |_____/   ) /\\\\  \\/.    |     \\/ \\        "
-    "/' /\\  \\    /\\\\  \\/.    | \\/    |   \n",
-    "    |.  |    // ___)_  //      / |: \\.        |     //  \\ ___  //  __'  "
-    "\\  |: \\.        | // ___)_  \n",
-    "    \\:  |   (:      \"||:  __   \\ |.  \\    /:  |    (:   _(  _|/   /  "
-    "\\\\  \\ |.  \\    /:  |(:      \"| \n",
-    "     \\__|    \\_______)|__|  \\___)|___|\\__/|___|     \\_______)(___/   "
-    " \\___)|___|\\__/|___| \\_______) \n",
+    " ___________  _______   _______   ___      ___       _______       __       ___      ___   _______  \n",
+    "(\"     _   \")/\"     \"| /\"      \\ |\"  \\    /\"  |     /\" _   \"|     /\"\"\\     |\"  \\    /\"  | /\"     \"| \n",
+    " )__/  \\\\__/(: ______)|:        | \\   \\  //   |    (: ( \\___)    /    \\     \\   \\  //   |(: ______) \n",
+    "    \\\\_ /    \\/    |  |_____/   ) /\\\\  \\/.    |     \\/ \\        /' /\\  \\    /\\\\  \\/.    | \\/    |   \n",
+    "    |.  |    // ___)_  //      / |: \\.        |     //  \\ ___  //  __'  \\  |: \\.        | // ___)_  \n",
+    "    \\:  |   (:      \"||:  __   \\ |.  \\    /:  |    (:   _(  _|/   /  \\\\  \\ |.  \\    /:  |(:      \"| \n",
+    "     \\__|    \\_______)|__|  \\___)|___|\\__/|___|     \\_______)(___/    \\___)|___|\\__/|___| \\_______) \n",
 };
+// clang-format on
+
 
 const char* const menu_choices[] = {"Play", "Options", "Potatisfläsk", "Exit"};
 
 static const int title_height = (int)(sizeof(title) / sizeof(char*));
 static const int title_width  = (int)strlen(title[0]);
 
-//const int outer_menu_width = 2 * title_width / 3 + 10;
+// const int outer_menu_width = 2 * title_width / 3 + 10;
 const int outer_menu_width = 77;
 
-//Note: This currently only works because the '\0' character knocks off one
-//character and the '◇' counts for two c characters in a string but 1 character
-//on the screen
+// Note: This currently only works because the '\0' character knocks off one
+// character and the '◇' counts for two c characters in a string but 1 character
+// on the screen
 const int inner_menu_width = outer_menu_width - 2;
 
 static const int inner_menu_height =
@@ -73,21 +72,21 @@ WINDOW* add_title(const Menu* menu)
 
 void print_menu_highlight(WINDOW* menu_win, int highlight, int x_align)
 {
-    //Compensate if menus contain non-ascii characters
+    // Compensate if menus contain non-ascii characters
     int const u8_ascii_diff = (int)strlen(menu_choices[highlight]) -
                               utf8_strlen(menu_choices[highlight]);
-    //The + 2 compensates for the '\0'-character and for the ◇ taking up to
-    //C-character but only one character on screen
+    // The + 2 compensates for the '\0'-character and for the ◇ taking up to
+    // C-character but only one character on screen
     int const total_len = inner_menu_width + u8_ascii_diff + 2;
     char* option        = (char*)malloc(total_len);
 
     int const sz =
         snprintf(option, total_len, u8"◇ %s", menu_choices[highlight]);
     if (sz > inner_menu_width + 1 || sz == -1) {
-        fprintf(stderr, //NOLINT
+        fprintf(stderr, // NOLINT
                 "Option string exceeded inner_menu_width or sprintf had an "
                 "error.\n Aborting...");
-        exit(-1); //NOLINT
+        exit(-1); // NOLINT
     }
     memset(option + sz, ' ', total_len - sz);
     option[total_len - 1] = '\0';
@@ -100,7 +99,7 @@ void print_menu_highlight(WINDOW* menu_win, int highlight, int x_align)
 
 bool refresh_menu_win(WINDOW* menu_win, const Menu* menu, int highlight)
 {
-    //Printing menu box
+    // Printing menu box
     wattroff(menu_win, A_STANDOUT);
     werase(menu_win);
     box(menu_win, 0, 0);
@@ -128,9 +127,10 @@ int get_widest_menu_choice()
     return max;
 }
 
+//Rework with the new window definition
 void verify_screen_size()
 {
-    //Should probably be removed/reworked at a later date
+    // Should probably be removed/reworked at a later date
     assert(COLS >= title_width);
     assert(COLS >= get_widest_menu_choice());
 }
--- utils/menu.h
+++ utils/menu.h
@@ -1,6 +1,59 @@
 #ifndef MENU_H
 #define MENU_H
 
+//! Enum used to specify text justification in menus
+enum Justification
+{
+    left_just,
+    center_just,
+    right_just
+};
+
+/*! \brief Macro to create a menu structure
+ *
+ *  This macro creates a menu structure to be used with other functions in this
+ * file for printing, etc.
+ *
+ *  \param name This will be the name of the menu and also determines the name
+ * of the struct created (will be ${name}_menu). There has to be a global char*
+ * array named the same thing containing the menu options, e.g. const char*
+ * const options[3] = [ "one", "two", "three"]
+ *
+ *  \param banner The name of a global char* array containing the banner (ascii
+ * art) to be printed above the menu's options. Every string in the array has to
+ * consist of the same number of unicode code points May be left NULL for no
+ * banner.
+ *
+ * \param min_width A minimum width for the options box, will be exceeded if the
+ * length of the options permits it
+ *
+ *  \param justification A constant of the Justification enum that indicates if
+ * the text should be left justified, right justified, or centered.
+ *
+ * \param left_pad A unicode code_point (in the form of a null-terminated
+ * string) to use for left padding
+ *
+ * \param right_pad A unicde code_point (in the
+ * form of a null-terminated string) to use for right padding
+ */
+#define make_menu_verbose(menu_name, menu_banner, min_width, justification,    \
+                          left_pad, right_pad)                                 \
+    struct name##_menu                                                         \
+    {                                                                          \
+        char const** const name;                                               \
+        int const title_height;                                                \
+        const char** const banner;                                             \
+        int const banner_height;                                               \
+    };                                                                         \
+    const struct name##_menu name##_menu = {                                   \
+        .name          = (menu_name),                                          \
+        .title_height  = (int)(sizeof(menu_name) / sizeof(char*)),             \
+        .banner        = (menu_banner),                                        \
+        .banner_height = (int)(sizeof(menu_banner) / sizeof(char*))};
+
+#define make_menu(name, banner, min_width)                                     \
+    make_menu_verbose(name, banner, min_width, left_just, " ", " ")
+
 typedef enum Choices
 {
     op_play,
@@ -10,7 +63,7 @@ typedef enum Choices
     op_N
 } Choices;
 
-typedef struct menu
+typedef struct
 {
     const int n_choices;
     const int height;
