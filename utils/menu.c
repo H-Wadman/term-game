b8cc0c03e48209b56602aced720d522f3adc0b87
Hannes Wadman <hannes.wadman@gmail.com>
2024-11-01 14:23:23 +0100

    Change get_dialogue_path so that it actually does as advertised, add abort it print_dia fails to open a file

--- utils/menu.c
+++ utils/menu.c
@@ -22,16 +22,20 @@ int const menu_box_width_offset = 2 + selection_offset;
 /*! Loads the path to the dialogue directory
  *
  *  \param buf A character buffer in which the path to the dialogue directory
- * will be stored. it is the responsibility of the caller that the buffer is of
- * sufficient size
+ * will be stored.
+ *
+ * \param sz An int specifying the size of the receiving buffer. If the receing
+ * buffer is too small, the program will abort
  */
 void get_dialogue_path(char* buf, int sz)
 {
-    if (!((int)strlen(SOURCE_DIR) < sz - 1)) {
+    int const extra_len = (int)strlen("/dialogue/");
+    if (!((int)strlen(SOURCE_DIR) + extra_len < sz - 1)) {
         fprintf(stderr, //NOLINT
                 "Buffer for dialogue path was not large enough, aborting...\n");
     }
     strcpy(buf, SOURCE_DIR);
+    strcat(buf, "/dialogue/");
 }
 
 WINDOW* add_banner(const struct menu* menu, WINDOW* menu_win)
@@ -363,8 +367,10 @@ int print_dia(const char* file_path, int width)
     if (!f) {
         fprintf( //NOLINT
             stderr,
-            "Error ocurred while opening file in print_dia with errno: %s\n",
-            strerror(errno));
+            "Error ocurred while opening file '%s' in print_dia with errno: "
+            "%s\n",
+            file_path, strerror(errno));
+        exit(1);
     }
     struct dia_print dia = {f, .path = file_path, .line = 1, .line_len = 0,
                             .width = width};
