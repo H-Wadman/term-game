ebc529c39a9aedec0752d9e5d2702f619c8aad92
Hannes Wadman <hannes.wadman@gmail.com>
2024-11-07 21:16:11 +0100

    Remove deprecated menu enums

--- utils/menu_constants.h
+++ utils/menu_constants.h
@@ -14,34 +14,14 @@
 
 extern_menu(start);
 
-enum Start_menu_choices
-{
-    start_play_ch,
-    start_options_ch,
-    start_exit_ch
-};
-
 extern_menu(options);
 
-enum Options_choices
-{
-    options_language_ch,
-    options_volume_ch,
-    options_colour_ch,
-    options_exit_ch
-};
 
 extern_menu(encounter);
 
-enum Encounter_menu_choices
-{
-    encounter_no_ch,
-    encounter_scream_ch,
-    encounter_fight_ch,
-    encounter_debug_ch,
-};
-
 extern_menu(glade);
+
+extern_menu(well);
 
 void initialise_menus();
 
